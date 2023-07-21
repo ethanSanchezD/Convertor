58d0781048d73a98c339fd354928708c8e9568db
ethanSanchezD <ethand.san@gmail.com>
2023-07-20 23:54:16 -0600

    Error in mvn build command, tried to fixed it

--- src/main/java/model/CurrencyConverter.java
+++ src/main/java/model/CurrencyConverter.java
@@ -6,58 +6,54 @@ import java.math.BigDecimal;
 import org.json.JSONObject;
 
 import controller.Controller;
-import okhttp3.Response;
+import okhttp3.*;
 
 /**
  * Class for currency conversion logic
  */
 public class CurrencyConverter {
-	
+
 	private Controller controller;
 
 	/**
-	 * Method that makes all the logic for currency conversion (it uses an API for the conversion)
-	 * @param quantityInput The quantity that user wants to convert
-	 * @param comboBoxFromString 'From' currency user option,  
-	 * @param comboBoxToString 'To' currency user option
+	 * Method that makes all the logic for currency conversion (it uses an API for
+	 * the conversion)
+	 * 
+	 * @param quantityInput      The quantity that user wants to convert
+	 * @param comboBoxFromString 'From' currency user option,
+	 * @param comboBoxToString   'To' currency user option
 	 * @return the conversion result
 	 */
 	public BigDecimal convert(BigDecimal quantityInput, String comboBoxFromString, String comboBoxToString) {
 		String urlString = "https://api.exchangerate.host/latest?base=" + comboBoxFromString.toUpperCase();
-		
+
 		String stringResponse = null;
 		try {
 			Response response = new RequestHttp().makeHttpRequest(urlString);
 			stringResponse = response.body().string();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		JSONObject jsonObject = new JSONObject(stringResponse);
 		JSONObject ratesObject = jsonObject.getJSONObject("rates");
 		BigDecimal rate = ratesObject.getBigDecimal(comboBoxToString.toUpperCase());
-		
+
 		BigDecimal result = rate.multiply(quantityInput);
-		
+
 		return result;
-		
-		
-		
-		
+
 	}
 
 	/**
 	 * Setter for controller field
-	 * @param controller The new controller 
+	 * 
+	 * @param controller The new controller
 	 */
 	public void setController(Controller controller) {
 		this.controller = controller;
-		
+
 	}
-	
-	
-	
 
 }
--- src/main/java/view/gui/ImagePanel.java
+++ src/main/java/view/gui/ImagePanel.java
@@ -7,22 +7,22 @@ import java.awt.geom.AffineTransform;
 /**
  * Class for put images in a window using a label
  */
-public class ImagePanel{
-	
+public class ImagePanel {
+
 	/**
 	 * Creates an image Icon with a JLabel height and width values
-	 * @param label The JLabel that is going to receive the image Icon
-	 * @param pathImage The path where the image is 
+	 * 
+	 * @param label     The JLabel that is going to receive the image Icon
+	 * @param pathImage The path where the image is
 	 * @return The Image icon with the defined scale
 	 */
-	  public Icon paintComponent(JLabel label, String pathImage) { 
-		  Icon image = new ImageIcon(new ImageIcon(getClass().getResource(pathImage))
-				  			.getImage().getScaledInstance(label.getWidth(),label.getHeight(),
-				  			Image.SCALE_DEFAULT));
-	  
-	  return image;
-	  
-	  
-	 }
+	public Icon paintComponent(JLabel label, String pathImage) {
+		Icon image = new ImageIcon(new ImageIcon(getClass().getResource(pathImage))
+				.getImage().getScaledInstance(label.getWidth(), label.getHeight(),
+						Image.SCALE_DEFAULT));
+
+		return image;
+
+	}
 
 }
